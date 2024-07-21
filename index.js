302f4ab3d4b6437bc7fb7c2764d366e2b3faa35a
felix maati <maatifelix001@gmail.com>
2024-07-21 12:15:48 +0300

    event listing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Example usage
+  const thankYouMessages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(thankYouMessages);
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usage
+  countDown(10);
+  // Output:
+  // 10
+  // 9
+  // 8
+  // 7
+  // 6
+  // 5
+  // 4
+  // 3
+  // 2
+  // 1
+  // 0
+    
